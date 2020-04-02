bb9817ee814c6cf4b62f9faad78f045121d75322
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-02 03:28:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,7 @@ end
 def assign_rooms(array)
   roomsArray = []
   array.each {|name| roomsArray << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+  return roomsArray
 end
 
 def printer
